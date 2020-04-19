7555578e2e46d44a809e6e799a8ba9fb1d9fbfee
EmilRejman <emilian.rejman@gmail.com>
2020-04-19 22:57:37 +0200

    changed layout of code

--- polls/urls.py
+++ polls/urls.py
@@ -3,15 +3,16 @@ from . import views
 from django.conf import settings
 
 # this urls are included with funcion 'include' to main catalog mysite\urls thats why app's urls are in main urls
-app_name = 'polls' #with this the django knows which {% url %} use when there are a lot of applications in the project
+app_name = 'polls'  # with this the django knows which {% url %} use when there are a lot of applications in the project
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'), #version with view
-    path('<int:question_id>/vote/', views.vote, name='vote'), #hard way of URL, withou usage of generic views
-    path('add_question/', views.AddQuestion, name = 'AddQuestion')
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),  # version with view
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # hard way of URL, withou usage of generic views
+    path('add_question/', views.AddQuestion, name='AddQuestion')
 ]
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     urlpatterns += staticfiles_urlpatterns()
